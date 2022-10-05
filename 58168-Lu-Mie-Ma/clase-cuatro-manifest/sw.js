77153153cb32bf83e96643cc7a8226b7868b2b46
Esteban Calabria <esteban.calabria@gmail.com>
2022-10-05 11:58:33 -0300

    wap

---
+++ 58168-Lu-Mie-Ma/clase-cuatro-manifest/sw.js
@@ -0,0 +1,4 @@
+
+self.addEventListener("install", ()=>{console.log("SW Install..")});
+self.addEventListener("activate", ()=>{console.log("SW Activate..")});
+self.addEventListener("fetch", ()=>{console.log("SW Fetch..")});
